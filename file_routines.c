f7146cbfc3687b6a991cd504d69d4466960b243a
Matheus Violaro Bellini <matheus.bellini02@gmail.com>
2022-05-07 15:16:44 -0300

    rename routine implemented

--- file_routines.c
+++ file_routines.c
@@ -19,3 +19,17 @@ void sys_remove_file(char* filepath) {
 	
 	system(strcat(command, filepath));
 }
+
+void sys_rename_file(char* filepath, char* new_name) {
+	char* command = new_string();
+	char win_cmd[] = "REN ";
+	char unix_cmd[] = "mv ";
+	char divider[] = " ";
+
+	if (IS_WIN)
+		strcpy(command, win_cmd);
+	else
+		strcpy(command, unix_cmd);
+	
+	system(strcat(strcat(strcat(command, filepath), divider), new_name));
+}
