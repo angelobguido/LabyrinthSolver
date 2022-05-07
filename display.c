992ca16736e35f382b772d9409512f7d28d6e34c
Matheus Violaro Bellini <matheus.bellini02@gmail.com>
2022-05-07 10:31:11 -0300

    display.h updated

--- display.c
+++ display.c
@@ -2,14 +2,6 @@
 #include <stdlib.h>
 #include "display.h"
 
-/* this directive is for the compiler to decide the user's operating system in order to use the correct
- * command in the system call contained in the function clear_screen()*/
-#ifndef __unix__
-	#define IS_WIN 0
-#elif defined(_WIN32) || defined(WIN32)
-	#define IS_WIN 1
-#endif
-
 // clears the console
 void clear_screen() {
 	if (IS_WIN)
