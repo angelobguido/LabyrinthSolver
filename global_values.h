56f8e7e7df03ea1a738c997fdfab4a9fe789b16a
Angelo Guido <angelobguido@gmail.com>
2022-06-22 12:56:21 -0300

    Correct IS_WIN problem

--- global_values.h
+++ global_values.h
@@ -3,7 +3,7 @@
 
 /* this directive is for the compiler to decide the user's operating system in order to use the correct
  * command in the system calls contained in the functions throughout the code*/
-#ifndef __unix__
+#ifdef __unix__
 	#define IS_WIN 0
 #elif _WIN32 || _WIN64
 	#define IS_WIN 1
